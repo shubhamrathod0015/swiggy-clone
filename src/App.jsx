563584067f526fd741c680494ac0529a0bb18bbd
shubhamrathod0015 <shubhamrathod664@gmail.com>
2025-02-21 15:41:59 +0530

    Header UI Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-
-import './App.css'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">SWIGGY CLONE </h1>
+     {/* <h1 className="text-3xl font-bold underline bg-amber-400">SWIGGY CLONE </h1> */}
+     <Header />
     </>
   )
 }
